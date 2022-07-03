e505c6b9f0ec5e0a09daf2609a9ac1ed9105afd2
Melanie H <mlh2197@columbia.edu>
2022-07-03 16:42:38 -0400

    chore: commit progress.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createHttpObservable } from '../common/util';
 
 @Component({
-  selector: 'about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+	selector: 'about',
+	templateUrl: './about.component.html',
+	styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+	}
 }
